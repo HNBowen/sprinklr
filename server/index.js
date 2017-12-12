232c495e92b229a800f5e6794f867cb8f8ed67fe
Hank <hbowen1123@gmail.com>
2017-12-12 17:11:20 -0600

    rendered hello world

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+const server = http.Server(app);
+
+
+
+//server static assets transpiled by webpack
+app.use('/static', express.static(path.join(__dirname, '../public')))
+
+//wildcard route, serve index.html
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+server.listen(8080, function() {
+  console.log('listening on port 8080')
+})
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var APP_DIR = path.resolve(__dirname, 'client/app');
+var BUILD_DIR = path.resolve(__dirname, 'public');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        loader : 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config;
