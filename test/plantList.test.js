25fd2944a2e857b49d3b96b7cc47eafb6dca3fee
Hank <hbowen1123@gmail.com>
2017-12-13 12:33:43 -0600

    Wrote tests and component for PlantList

--- test/plantList.test.js
+++ test/plantList.test.js
@@ -3,11 +3,18 @@ import {shallow} from 'enzyme'
 import {shallowToJson} from 'enzyme-to-json'
 
 import PlantList from '../client/app/components/PlantList.jsx'
+import PlantTile from '../client/app/components/PlantTile.jsx'
 
 describe('PlantList', function() {
   //it should render correctly
   it('should render correctly', function() {
-    const wrapper = shallow(<PlantList />);
+    const wrapper = shallow(<PlantList plants={[]}/>);
     jestExpect(shallowToJson(wrapper)).toMatchSnapshot();
+  })
+
+  //it should render PlantTiles from props
+  it('should render PlantTiles from props', function() {
+    const wrapper = shallow(<PlantList plants={[{name: 'plant1', img: 'image'},{name: 'plant2', img: 'image'}]}/>)
+    expect(wrapper.find(PlantTile)).to.have.length(2);
   })
 })
