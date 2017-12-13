ac9333356c5ecbcc9fe9b169c3648d740f8be086
Hank <hbowen1123@gmail.com>
2017-12-13 09:42:24 -0600

    Create component and test files

---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import {shallowToJson} from 'enzyme-to-json'
+
+import App from '../../../client/app/components/App.jsx'
+
+describe('App', function() {
+  
+
+  it('should render correctly', function() {
+    
+    expect(1).toBe(1);
+  })
+})
