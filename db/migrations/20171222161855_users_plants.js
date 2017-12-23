3730d86eb6c550aa6e4133c62a4db58a8b4dca93
Hank <hbowen1123@gmail.com>
2017-12-22 23:11:11 -0600

    Fixed crazy migrations error

---
+++ db/migrations/20171222161855_users_plants.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('password').notNullable();
+  }).then(function(table) {
+    return knex.schema.createTable('plants', function(table) {
+    table.increments();
+    table.string('name');
+    table.string('image');
+    table.date('dateAdded');
+    table.date('lastWatered');
+    table.integer('user_id').notNullable().references('id').inTable('users');
+    })
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('plants').then(function() {
+    return knex.schema.dropTable('users')
+  });
+};
