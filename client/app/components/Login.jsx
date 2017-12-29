834170a27233c87141453370a051f1504034a6b3
Hank <hbowen1123@gmail.com>
2017-12-29 12:02:13 -0600

    Wrote button click handler tests for login, added proptypes to Login

--- client/app/components/Login.jsx
+++ client/app/components/Login.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Login = () => {
+const Login = (props) => {
   return (
       <div>
         <form>
           username: <input type="text" name="username"></input>
           password: <input type="text" name="password"></input>
-          <button>Login</button>
+          <button onClick={props.handleLogin}>Login</button>
         </form>
-        <button>Register</button>
+        <button onClick={props.handleRegister}>Register</button>
       </div>
     )
+}
+
+Login.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  handleRegister: PropTypes.func.isRequired
 }
 
 export default Login
--- test/client/login.test.js
+++ test/client/login.test.js
@@ -7,12 +7,12 @@ import Login from '../../client/app/components/Login.jsx'
 describe('Login', function() {
 
   it('should render correctly', function() {
-    let wrapper = shallow(<Login />)
+    let wrapper = shallow(<Login handleLogin={()=>{}} handleRegister={()=>{}}/>)
     jestExpect(shallowToJson(wrapper)).toMatchSnapshot();
   })
 
   it('should render with an input field for username', function() {
-    let wrapper = shallow(<Login />)
+    let wrapper = shallow(<Login handleLogin={()=>{}} handleRegister={()=>{}}/>)
 
     let usernameFieldWrapper = wrapper.find('input').get(0)
 
@@ -21,7 +21,7 @@ describe('Login', function() {
   })
 
   it('should render with an input field for password', function() {
-    let wrapper = shallow(<Login />)
+    let wrapper = shallow(<Login handleLogin={()=>{}} handleRegister={()=>{}}/>)
 
     let passwordFieldWrapper = wrapper.find('input').get(1);
 
@@ -31,7 +31,7 @@ describe('Login', function() {
 
   it('should render with Login and Register buttons', function() {
 
-    let wrapper = shallow(<Login />)
+    let wrapper = shallow(<Login handleLogin={()=>{}} handleRegister={()=>{}}/>)
 
     let loginButtonWrapper = wrapper.find('button').get(0);
     let registerButtonWrapper = wrapper.find('button').get(1);
@@ -39,6 +39,19 @@ describe('Login', function() {
     expect(loginButtonWrapper.props.children).to.equal('Login')
     expect(registerButtonWrapper.props.children).to.equal('Register')
 
+  })
+
+  it('should call click handlers when buttons are clicked', function() {
+    let loginMock = jest.fn();
+    let registerMock = jest.fn();
+
+    let wrapper = shallow(<Login handleLogin={loginMock} handleRegister={registerMock} />)
+
+    wrapper.find('button').at(0).simulate('click')
+    wrapper.find('button').at(1).simulate('click')
+
+    expect(loginMock.mock.calls.length).to.equal(1);
+    expect(registerMock.mock.calls.length).to.equal(1);
   })
 
 })
